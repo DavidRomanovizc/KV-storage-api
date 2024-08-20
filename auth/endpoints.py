5a821a71b7ffc40f72fa833d80f73fa915996635
dromanov <dtdzhalaev@gmail.com>
2024-08-20 20:24:36 +0300

    ♻️ auth part move to utils from endpoints

--- auth/endpoints.py
+++ auth/endpoints.py
@@ -1,8 +1,3 @@
-import datetime
-from typing import (
-    Any,
-)
-
 from jose import (
     JWTError,
     jwt,
@@ -19,10 +14,13 @@ from starlette.requests import (
 from starlette.responses import (
     JSONResponse,
 )
-from starlette.routing import Route
+from starlette.routing import (
+    Route,
+)
 
 import config
 import dto
+import utils
 
 __all__ = (
     "setup_routes",
@@ -37,29 +35,6 @@ def setup_routes() -> list[Route]:
     return routes
 
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return plain_password == hashed_password
-
-
-def authenticate_user(
-        fake_db: dict[str, dict[str, Any]],
-        username: str,
-        password: str,
-) -> dict[str, Any] | None:
-    user = fake_db.get(username)
-    if not user or not verify_password(password, user["password"]):
-        return None
-    return user
-
-
-def create_access_token(data: dict, expires_delta: datetime.timedelta) -> str:
-    to_encode = data.copy()
-    expire = datetime.datetime.now(datetime.timezone.utc) + expires_delta
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, config.SECRET_KEY, algorithm=config.ALGORITHM)
-    return encoded_jwt
-
-
 async def login(request: Request) -> JSONResponse:
     body = await request.json()
     try:
@@ -68,12 +43,12 @@ async def login(request: Request) -> JSONResponse:
         raise HTTPException(
             detail="Invalid login data", status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
         )
-    user = authenticate_user(config.fake_users_db, token_request.username, token_request.password)
+    user = utils.authenticate_user(config.fake_users_db, token_request.username, token_request.password)
     if not user:
         raise HTTPException(detail="Invalid credentials", status_code=401)
 
     access_token_expires = config.get_token_expiration()
-    access_token = create_access_token(
+    access_token = utils.create_access_token(
         data={"sub": user["username"]}, expires_delta=access_token_expires
     )
     token = dto.TokenResponse(token=access_token)
